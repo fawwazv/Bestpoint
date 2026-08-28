eec013972d74b7925d356836d7e220406f754ae3
Fawwazul <mhmdfwwz7@gmail.com>
2026-08-28 13:33:24 +0700

    change structure project and add flash message

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const express = require('express');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
-const Joi = require('joi');
-const wrapAsync = require('./utils/wrapAsync');
+const session = require('express-session');
+const flash = require('connect-flash')
 const ErrorHandler = require('./utils/ErrorHandler');
 const app = express();
-const path = require('path');
-
-//models
-const Place = require('./models/place');
-const Review = require('./models/review');
-
-//schemas
-const { placeSchema } = require('./schemas/place');     
-const { reviewSchema } = require('./schemas/review');                                    
+const path = require('path');                                 
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/bestpoints')
@@ -32,80 +24,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-
-const validatePlace = (req, res, next) => {
-    const { error } = placeSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(', ');
-        return next(new ErrorHandler(msg, 400));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'this-is-a-secret-key',
+    resave: false,
+    saveUnitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
+}))
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
     next();
-};
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(', ');
-        return next(new ErrorHandler(msg, 400));
-    }
-    next();
-};
+});
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/places', wrapAsync(async (req, res) => {
-    const places = await Place.find({});
-    res.render('places/index', { places });
-}));
-
-app.get('/places/create', (req, res) => {
-    res.render('places/create');
-});
-
-app.post('/places', validatePlace, wrapAsync(async (req, res, next) => {
-    const place = new Place(req.body.place);
-    await place.save();
-    res.redirect(`/places`);
-}));
-
-app.get('/places/:id', wrapAsync(async (req, res) => {
-    const place = await Place.findById(req.params.id).populate('reviews');
-    res.render('places/show', { place });
-}));
-
-app.get('/places/:id/edit', wrapAsync(async (req, res) => {
-    const place = await Place.findById(req.params.id);
-    res.render('places/edit', { place });
-}));
-
-app.put('/places/:id', validatePlace, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const place = await Place.findByIdAndUpdate(req.params.id, {...req.body.place});
-    res.redirect('/places');
-}));
-
-app.delete('/places/:id', wrapAsync(async (req, res) => {
-    await Place.findByIdAndDelete(req.params.id);
-    res.redirect('/places');
-}));
-
-app.post('/places/:id/reviews',validateReview, wrapAsync(async (req, res) => {
-    const review = new Review(req.body.review);
-    const place = await Place.findById(req.params.id);
-    place.reviews.push(review);
-    await review.save();
-    await place.save();
-    res.redirect(`/places/${req.params.id}`);
-}));
-
-app.delete('/places/:place_id/reviews/:review_id', wrapAsync(async (req, res) => {
-    const { place_id, review_id } = req.params;
-    await Place.findByIdAndUpdate(place_id, { $pull: { reviews: review_id } });
-    await Review.findByIdAndDelete(review_id);
-    res.redirect(`/places/${place_id}`);
-}));
+app.use('/places', require('./routes/places'));
+app.use('/places/:place_id/reviews', require('./routes/reviews'));
 
 app.all('/{*splat}', (req, res, next) => {
     next(new ErrorHandler('Page Not Found', 404));
---
+++ routes/reviews.js
@@ -0,0 +1,41 @@
+//models
+const express = require('express');
+const Place = require('../models/place');
+const wrapAsync = require('../utils/wrapAsync');
+const ErrorHandler = require('../utils/ErrorHandler');
+const isValidObjectId = require('../middleware/isValidObjectId');
+const Review = require('../models/review');
+
+const router = express.Router({ mergeParams: true });
+
+//schemas   
+const { reviewSchema } = require('../schemas/review'); 
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(', ');
+        return next(new ErrorHandler(msg, 400));
+    }
+    next();
+};
+
+router.post('/', isValidObjectId('/places'), validateReview, wrapAsync(async (req, res) => {
+    const review = new Review(req.body.review);
+    const place = await Place.findById(req.params.place_id);
+    place.reviews.push(review);
+    await review.save();
+    await place.save();
+    req.flash('success_msg', 'Review added successfully');
+    res.redirect(`/places/${req.params.place_id}`);
+}));
+
+router.delete('/:review_id', isValidObjectId('/places'), wrapAsync(async (req, res) => {
+    const { place_id, review_id } = req.params;
+    await Place.findByIdAndUpdate(place_id, { $pull: { reviews: review_id } });
+    await Review.findByIdAndDelete(review_id);
+    req.flash('success_msg', 'Review deleted successfully');
+    res.redirect(`/places/${place_id}`);
+}));
+
+module.exports = router;
